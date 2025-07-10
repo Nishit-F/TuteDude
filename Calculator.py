289d23c96966166a82350f44ab95a10dc687a788
Nishit-F <nishitfulzele05@gmail.com>
2025-07-10 11:11:06 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+a = input ('Frist Number: ')
+b = input ('Second Number: ')
+a = int (a)
+b = int (b)
+
+
+print ('Addition: ', a+b ,'\n'
+       'Subtraction: ', a-b ,'\n'
+       'Multiplication: ', a*b ,'\n'
+       'Division: ', a/b )
---
+++ Name.py
@@ -0,0 +1,3 @@
+d = input ('Enter first Name: ')
+e = input ('Enter Last Name: ')
+print('Hello, ',d ,'\t' + e ,'nice to meet you.')
